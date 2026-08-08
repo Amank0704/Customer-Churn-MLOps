9923c41d6a84b2eb759da78d5c1941199d7dbda2
Amank0704 <akshirsagar0704@gmail.com>
2026-08-08 12:21:55 +0530

    updated file

--- src/config.py
+++ src/config.py
@@ -1,25 +1,62 @@
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# # Load environment variables from .env file
+# # PostgreSQL
+# POSTGRES_USER = os.getenv("POSTGRES_USER")
+# POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "churn_pass")
+# POSTGRES_DB = os.getenv("POSTGRES_DB", "churn_db")
+# POSTGRES_HOST = os.getenv("POSTGRES_HOST","localhost")
+# POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+# DATABASE_URL = (
+#     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+#     f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+# )
+
+
+# # MLflow
+# MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
+
+
+# # Paths
+# RAW_DATA_PATH = "data/raw/telco_churn.csv"
+# PROCESSED_DATA_PATH = "data/processed/"
+# MODEL_DIR = "models/"
+
+# TARGET_COLUMN = "Churn"
+
+
+
+
+
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Load environment variables from .env file
 # PostgreSQL
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "churn_pass")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "churn_db")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST","localhost")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = (
-    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
-    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-)
+if not DATABASE_URL:
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "churn_pass")
+    POSTGRES_DB = os.getenv("POSTGRES_DB", "churn_db")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
+    DATABASE_URL = (
+        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+        f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    )
 
 # MLflow
-MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
-
+MLFLOW_TRACKING_URI = os.getenv(
+    "MLFLOW_TRACKING_URI",
+    "http://localhost:5000"
+)
 
 # Paths
 RAW_DATA_PATH = "data/raw/telco_churn.csv"
